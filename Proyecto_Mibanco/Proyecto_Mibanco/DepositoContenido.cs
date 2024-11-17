0011c257f2936d035c43b02c6b9033a91500834d
FypLol <105802763+FypLol@users.noreply.github.com>
2024-11-17 12:30:34 -0500

    Add files via upload
    Versión 1.0

---
+++ Proyecto_Mibanco/Proyecto_Mibanco/DepositoContenido.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Mibanco
+{
+    public partial class DepositoContenido : Form
+    {
+        public DepositoContenido()
+        {
+            InitializeComponent();
+        }
+        #region
+        private void btn10Retirar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Transacción realizada");
+        }
+
+        private void btn20Retirar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Transacción realizada");
+        }
+
+        private void btn50Retirar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Transacción realizada");
+        }
+
+        private void btn100Retirar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Transacción realizada");
+        }
+
+        private void btn1000Retirar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Transacción realizada");
+        }
+        #endregion
+
+        private void btnNumero1_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "1";
+        }
+
+        private void btnNumero2_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "2";
+        }
+
+        private void btnNumero3_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "3";
+        }
+
+        private void btnNumero4_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "4";
+        }
+
+        private void btnNumero5_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "5";
+        }
+
+        private void btnNumero6_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "6";
+        }
+
+        private void btnNumero7_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "7";
+        }
+
+        private void btnNumero8_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "8";
+        }
+
+        private void btnNumero9_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "9";
+        }
+
+        private void btnNumero0_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadDepositar.Text = rtboxCantidadDepositar.Text + "0";
+        }
+
+        private void btnBorrarNumero_Click(object sender, EventArgs e)
+        {
+            string cadena = rtboxCantidadDepositar.Text;
+            cadena = cadena.Substring(0, cadena.Length - 1);
+            rtboxCantidadDepositar.Text = cadena;
+        
+        }
+
+        private void btnEnterDepositar_Click(object sender, EventArgs e)
+        {
+         MessageBox.Show("Su deposito de S/" + rtboxCantidadDepositar.Text + " se realizo.");
+            
+        }
+    }
+}
---
+++ Proyecto_Mibanco/Proyecto_Mibanco/RetirarContenido.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Mibanco
+{
+    public partial class RetirarContenido : Form
+    {
+        public RetirarContenido()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_Monto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSaldoUsuario_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnNumero1_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "1";
+        }
+
+        private void btnNumero2_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "2";
+        }
+
+        private void btnNumero3_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "3";
+        }
+
+        private void btnNumero4_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "4";
+        }
+
+        private void btnNumero5_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "5";
+        }
+
+        private void btnNumero6_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "6";
+        }
+
+        private void btnNumero7_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "7";
+        }
+
+        private void btnNumero8_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "8";
+        }
+
+        private void btnNumero9_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "9";
+        }
+
+        private void btnNumero0_Click(object sender, EventArgs e)
+        {
+            rtboxCantidadRetirar.Text = rtboxCantidadRetirar.Text + "0";
+        }
+
+        private void btnEnterRetirar_Click(object sender, EventArgs e)
+        {
+            int montoRetirar = int.Parse(rtboxCantidadRetirar.Text);
+            if (montoRetirar <= 1000)
+            {
+                MessageBox.Show("Su deposito de S/" + rtboxCantidadRetirar.Text + " se realizo.");
+            }
+            else
+            {
+                MessageBox.Show("No tiene fondos suficientes");
+            }
+        }
+
+        private void btnBorrarNumero_Click(object sender, EventArgs e)
+        {
+            string cadena = rtboxCantidadRetirar.Text;
+            cadena = cadena.Substring(0, cadena.Length - 1);
+            rtboxCantidadRetirar.Text = cadena;
+        }
+    }
+}
